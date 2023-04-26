b6c34a6327565cf80cc38782bc8019f2ed5e3e9c
kafi ahmed <kafyahmedfarabi@gmail.com>
2023-04-26 18:13:30 +0600

    all

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import Hero from '../COMPONENTS/Hero'
 
-import item1 from '../Images/Image.png'
-
-import { data } from '../Db/data'
 import Blogpost from '../COMPONENTS/Blogpost'
-import Newpost from './Newpost'
 import Newslatters from '../COMPONENTS/Newslatters'
 
 
